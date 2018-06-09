4427a8964fb210b19cca5f7808a16bd33280079d
Prem <karnprem@cronj.com>
2018-06-09 19:55:54 +0530

    Replaced URL

--- src/server/db/mongoose.js
+++ src/server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://premkarn:premkarn1@ds153890.mlab.com:53890/heroku_q3bds973');
 
 module.exports = {
   mongoose
